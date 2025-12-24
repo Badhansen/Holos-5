68ce4a78cfbadf25254509d3b1d7673bb7b2633c
Aaron McPherson <33136427+aaron-mcpherson@users.noreply.github.com>
2025-12-24 12:01:46 -0700

    Refactor AnimalGroupFactory; clean up user dictionary
    Refactored AnimalGroupFactory.cs for clarity and robustness: made the AutoMapper dependency nullable, used null-conditional mapping, and added XML documentation to constructors and methods. Simplified DI constructor logic. Removed all custom user dictionary entries from Holos.sln.DotSettings, so custom terms will no longer be ignored by the spell checker.

--- H.Core/Factories/Animals/AnimalGroupFactory.cs
+++ H.Core/Factories/Animals/AnimalGroupFactory.cs
@@ -5,57 +5,78 @@ using Prism.Ioc;
 
 namespace H.Core.Factories.Animals;
 
+/// <summary>
+/// Factory for creating <see cref="AnimalGroupDto"/> instances with optional AutoMapper support.
+/// </summary>
 public class AnimalGroupFactory : IAnimalGroupFactory
 {
     #region Fields
 
-    private readonly IMapper _animalGroupDtoToAnimalGroupDtoMapper;
+    /// <summary>
+    /// Optional mapper for copying data between <see cref="AnimalGroupDto"/> instances.
+    /// Will be null if the factory is created without dependency injection.
+    /// </summary>
+    private readonly IMapper? _animalGroupDtoToAnimalGroupDtoMapper;
 
     #endregion
 
     #region Constructors
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AnimalGroupFactory"/> class without dependency injection.
+    /// The mapper will be unavailable, so <see cref="CreateDtoFromDtoTemplate"/> will skip mapping operations.
+    /// </summary>
     public AnimalGroupFactory()
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AnimalGroupFactory"/> class with dependency injection.
+    /// </summary>
+    /// <param name="containerProvider">The container provider used to resolve the AutoMapper instance.</param>
     public AnimalGroupFactory(IContainerProvider containerProvider)
     {
-        if (containerProvider != null)
-        {
-            _animalGroupDtoToAnimalGroupDtoMapper = containerProvider.Resolve<IMapper>(nameof(AnimalGroupDtoToAnimalGroupDtoMapper));
-        }
-        else
-        {
-            throw new ArgumentNullException(nameof(containerProvider));
-        }
+        _animalGroupDtoToAnimalGroupDtoMapper = containerProvider.Resolve<IMapper>(nameof(AnimalGroupDtoToAnimalGroupDtoMapper));
     }
 
     #endregion
 
     #region Public Methods
-    
+
+    /// <summary>
+    /// Creates a new <see cref="AnimalGroupDto"/> instance with default values.
+    /// </summary>
+    /// <returns>A new <see cref="AnimalGroupDto"/> instance.</returns>
     public AnimalGroupDto CreateDto()
     {
         return new AnimalGroupDto();
     }
 
+    /// <summary>
+    /// Creates a new <see cref="AnimalGroupDto"/> instance for the specified farm.
+    /// </summary>
+    /// <param name="farm">The farm context (currently unused).</param>
+    /// <returns>A new <see cref="AnimalGroupDto"/> instance.</returns>
     public AnimalGroupDto CreateDto(Farm farm)
     {
         return new AnimalGroupDto();
     }
 
+    /// <summary>
+    /// Creates a new <see cref="AnimalGroupDto"/> instance by copying data from a template.
+    /// If AutoMapper is available, properties will be copied from the template; otherwise, default values are used.
+    /// </summary>
+    /// <param name="template">The template DTO to copy data from.</param>
+    /// <returns>A new <see cref="AnimalGroupDto"/> instance with data copied from the template if mapping is available.</returns>
     public IDto CreateDtoFromDtoTemplate(IDto template)
     {
         var result = new AnimalGroupDto();
 
-        if (_animalGroupDtoToAnimalGroupDtoMapper != null)
-        {
-            _animalGroupDtoToAnimalGroupDtoMapper.Map(template, result);
-        }
+        // Use null-conditional operator to safely invoke mapping only if mapper is available
+        _animalGroupDtoToAnimalGroupDtoMapper?.Map(template, result);
 
         return result;
-    } 
+    }
 
     #endregion
 }

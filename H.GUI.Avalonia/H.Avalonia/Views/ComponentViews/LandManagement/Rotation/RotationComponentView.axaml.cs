30654f4cb3e817423fe976b60737e4e1622c6578
Aaron McPherson <33136427+aaron-mcpherson@users.noreply.github.com>
2026-01-20 10:33:13 -0700

    Add reusable AdvancedOptionsToggleView user control
    Refactored advanced options toggle into a new reusable AdvancedOptionsToggleView component with customizable title and description. Updated FieldComponentView and RotationComponentView to use this control, improving code reuse and consistency. Added styled properties and design-time defaults for toggle state.

--- H.GUI.Avalonia/H.Avalonia/Views/ComponentViews/LandManagement/Rotation/RotationComponentView.axaml.cs
+++ H.GUI.Avalonia/H.Avalonia/Views/ComponentViews/LandManagement/Rotation/RotationComponentView.axaml.cs
@@ -1,14 +1,34 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace H.Avalonia.Views.ComponentViews.LandManagement.Rotation;
 
 public partial class RotationComponentView : UserControl
 {
+    #region Properties
+
+    public static readonly StyledProperty<bool> ShowAdvancedOptionsProperty =
+        AvaloniaProperty.Register<RotationComponentView, bool>(nameof(ShowAdvancedOptions), defaultValue: false);
+
+    public bool ShowAdvancedOptions
+    {
+        get => GetValue(ShowAdvancedOptionsProperty);
+        set => SetValue(ShowAdvancedOptionsProperty, value);
+    }
+
+    #endregion
+
     #region Constructors
 
     public RotationComponentView()
     {
         InitializeComponent();
+        
+        // Set default value for design time
+        if (Design.IsDesignMode)
+        {
+            ShowAdvancedOptions = true;
+        }
     } 
 
     #endregion
